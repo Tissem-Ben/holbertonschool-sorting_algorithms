ea7258b4538fdb0ea073bcea87e4269d78b793c3
Tissem-Ben <t.bendahma@gmail.com>
2024-08-09 16:15:27 +0200

    Update 1-main.c, sort.h and remove obsolete 'insertion' binary

--- 1-main.c
+++ 1-main.c
@@ -1,56 +1,43 @@
+#include "sort.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "sort.h"
 
-/**
- ** create_listint - Creates a doubly linked list from an array of integers
- *
- * @array: Array to convert to a doubly linked list
- * @size: Size of the array
- *
- * Return: Pointer to the first element of the created list. NULL on failure
- */
-listint_t *create_listint(const int *array, size_t size)
+listint_t *create_node(int value)
 {
-	listint_t *list;
-	listint_t *node;
-	int *tmp;
-
-	list = NULL;
-	while (size--)
-	{
-		node = malloc(sizeof(*node));
-		if (!node)
-			return (NULL);
-		tmp = (int *)&node->n;
-		*tmp = array[size];
-		node->next = list;
-		node->prev = NULL;
-		list = node;
-		if (list->next)
-			list->next->prev = list;
-	}
-	return (list);
+	listint_t *new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	return (new_node);
 }
 
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
 int main(void)
 {
-	listint_t *list;
-	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-	size_t n = sizeof(array) / sizeof(array[0]);
+	listint_t *head = create_node(3);
+	head->next = create_node(1);
+	head->next->prev = head;
+	head->next->next = create_node(2);
+	head->next->next->prev = head->next;
 
-	list = create_listint(array, n);
-	if (!list)
-		return (1);
-	print_list(list);
-	printf("\n");
-	insertion_sort_list(&list);
-	printf("\n");
-	print_list(list);
-	return (0);
+	printf("Original list:\n");
+	print_list(head);
+
+	insertion_sort_list(&head);
+
+	printf("Sorted list:\n");
+	print_list(head);
+
+	/* Free the list nodes */
+	listint_t *tmp;
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+
+	return 0;
 }
+
